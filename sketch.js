4130c30746eff52d75bbfaf1a055a9deaf99c1ba
vincent950928-eng <vincent950928@gmail.com>
2025-11-15 12:57:38 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,140 +1,94 @@
-let objs = [];
-let colors = ['#f71735', '#f7d002', '#1A53C0', '#232323'];
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-function setup() {
-	let c = createCanvas(800, 600);
-	rectMode(CENTER);
-	objs.push(new DynamicShape());
-}
+    <title>Sketch</title>
 
-function draw() {
-	background(0);
-	for (let i of objs) {
-		i.run();
-	}
+    <link rel="stylesheet" type="text/css" href="style.css">
 
-	if (frameCount % int(random([15, 30])) == 0) {
-		let addNum = int(random(1, 30));
-		for (let i = 0; i < addNum; i++) {
-			objs.push(new DynamicShape());
-		}
-	}
-	for (let i = 0; i < objs.length; i++) {
-		if (objs[i].isDead) {
-			objs.splice(i, 1);
-		}
-	}
-}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&family=Rye&display=swap" rel="stylesheet">
 
-function easeInOutExpo(x) {
-	return x === 0 ? 0 :
-		x === 1 ?
-		1 :
-		x < 0.5 ? Math.pow(2, 20 * x - 10) / 2 :
-		(2 - Math.pow(2, -20 * x + 10)) / 2;
-}
+    <script src="libraries/p5.min.js"></script>
+    <script src="libraries/p5.sound.min.js"></script>
+  </head>
 
-class DynamicShape {
-	constructor() {
-		this.x = random(0.3, 0.7) * width;
-		this.y = random(0.3, 0.7) * height;
-		this.reductionRatio = 1;
-		this.shapeType = int(random(4));
-		this.animationType = 0;
-		this.maxActionPoints = int(random(2, 5));
-		this.actionPoints = this.maxActionPoints;
-		this.elapsedT = 0;
-		this.size = 0;
-		this.sizeMax = width * random(0.01, 0.05);
-		this.fromSize = 0;
-		this.init();
-		this.isDead = false;
-		this.clr = random(colors);
-		this.changeShape = true;
-		this.ang = int(random(2)) * PI * 0.25;
-		this.lineSW = 0;
-	}
+  <body>
+    <div id="menuTrigger" aria-hidden="true"></div>
+    <nav id="sideMenu" aria-label="側邊選單">
+      <ul>
+        <li><a href="#" id="firstUnitWorks">第一單元作品</a></li>
+        <li><a href="#" id="firstUnitNotes">第一單元講義</a></li>
+        <li><a href="#" id="quizSystem">測驗系統</a></li>
+        <li><a href="#" id="quizNotes">測驗卷筆記</a></li>
+        <li><a href="#" id="projectNotes">作品筆記</a></li>
+        <li class="submenu-parent">
+          <a href="#" id="tkuLink">淡江大學</a>
+          <ul class="submenu">
+            <li><a href="#" id="tkuEtLink">教育科技學系</a></li>
+          </ul>
+        </li>
+        <li><a href="#" id="backToHome">回到首頁</a></li> 
+      </ul>
+    </nav>
 
-	show() {
-		push();
-		translate(this.x, this.y);
-		if (this.animationType == 1) scale(1, this.reductionRatio);
-		if (this.animationType == 2) scale(this.reductionRatio, 1);
-		fill(this.clr);
-		stroke(this.clr);
-		strokeWeight(this.size * 0.05);
-		if (this.shapeType == 0) {
-			noStroke();
-			circle(0, 0, this.size);
-		} else if (this.shapeType == 1) {
-			noFill();
-			circle(0, 0, this.size);
-		} else if (this.shapeType == 2) {
-			noStroke();
-			rect(0, 0, this.size, this.size);
-		} else if (this.shapeType == 3) {
-			noFill();
-			rect(0, 0, this.size * 0.9, this.size * 0.9);
-		} else if (this.shapeType == 4) {
-			line(0, -this.size * 0.45, 0, this.size * 0.45);
-			line(-this.size * 0.45, 0, this.size * 0.45, 0);
-		}
-		pop();
-		strokeWeight(this.lineSW);
-		stroke(this.clr);
-		line(this.x, this.y, this.fromX, this.fromY);
-	}
+    <!-- IFrame overlay: initially hidden -->
+    <div id="iframeOverlay" role="dialog" aria-hidden="true">
+      <div id="iframeContainer">
+        <button id="iframeClose" aria-label="關閉">✕</button>
+        <iframe id="contentFrame" src="" frameborder="0" allowfullscreen></iframe>
+      </div>
+    </div>
 
-	move() {
-		let n = easeInOutExpo(norm(this.elapsedT, 0, this.duration));
-		if (0 < this.elapsedT && this.elapsedT < this.duration) {
-			if (this.actionPoints == this.maxActionPoints) {
-				this.size = lerp(0, this.sizeMax, n);
-			} else if (this.actionPoints > 0) {
-				if (this.animationType == 0) {
-					this.size = lerp(this.fromSize, this.toSize, n);
-				} else if (this.animationType == 1) {
-					this.x = lerp(this.fromX, this.toX, n);
-					this.lineSW = lerp(0, this.size / 5, sin(n * PI));
-				} else if (this.animationType == 2) {
-					this.y = lerp(this.fromY, this.toY, n);
-					this.lineSW = lerp(0, this.size / 5, sin(n * PI));
-				} else if (this.animationType == 3) {
-					if (this.changeShape == true) {
-						this.shapeType = int(random(5));
-						this.changeShape = false;
-					}
-				}
-				this.reductionRatio = lerp(1, 0.3, sin(n * PI));
-			} else {
-				this.size = lerp(this.fromSize, 0, n);
-			}
-		}
+    <script>
+      (function(){
+        const url1 = 'https://vincent950928-eng.github.io/20251020/';
+        const url2 = 'https://hackmd.io/@oFfjbdXYRC-RDOnVZOnbpw/S1NgwmCsee';
+        const url3 = 'https://vincent950928-eng.github.io/20251103-/';
+        const url4 = 'https://hackmd.io/@oFfjbdXYRC-RDOnVZOnbpw/rkJkCvrxbx';
+        const urlTku = 'https://www.tku.edu.tw/';
+        const urlTkuEt = 'https://www.et.tku.edu.tw/';
+        const btn1 = document.getElementById('firstUnitWorks');
+        const btn2 = document.getElementById('firstUnitNotes');
+        const btn3 = document.getElementById('quizSystem');
+        const btn4 = document.getElementById('quizNotes');
+        const btn5 = document.getElementById('projectNotes');
+        const btnTku = document.getElementById('tkuLink');
+        const btnTkuEt = document.getElementById('tkuEtLink');
+        const overlay = document.getElementById('iframeOverlay');
+        const iframe = document.getElementById('contentFrame');
+        const closeBtn = document.getElementById('iframeClose');
 
-		this.elapsedT++;
-		if (this.elapsedT > this.duration) {
-			this.actionPoints--;
-			this.init();
-		}
-		if (this.actionPoints < 0) {
-			this.isDead = true;
-		}
-	}
+        function openIframe(u){
+          iframe.src = u;
+          overlay.style.display = 'flex';
+          overlay.setAttribute('aria-hidden','false');
+          document.body.style.overflow = 'hidden';
+        }
 
-	run() {
-		this.show();
-		this.move();
-	}
+        function closeIframe(){
+          iframe.src = '';
+          overlay.style.display = 'none';
+          overlay.setAttribute('aria-hidden','true');
+          document.body.style.overflow = '';
+        }
 
-	init() {
-		this.elapsedT = 0;
-		this.fromSize = this.size;
-		this.toSize = this.sizeMax * random(0.5, 1.5);
-		this.fromX = this.x;
-		this.toX = this.fromX + (width / 10) * random([-1, 1]) * int(random(1, 4));
-		this.fromY = this.y;
-		this.toY = this.fromY + (height / 10) * random([-1, 1]) * int(random(1, 4));
-		this.animationType = int(random(3));
-		this.duration = random(50, 100);
-	}
-}
+        btn1.addEventListener('click', function(e){ e.preventDefault(); openIframe(url1); });
+        btn2.addEventListener('click', function(e){ e.preventDefault(); openIframe(url2); });
+        btn3.addEventListener('click', function(e){ e.preventDefault(); openIframe(url3); });
+        btn4.addEventListener('click', function(e){ e.preventDefault(); openIframe(url4); });
+        btn5.addEventListener('click', function(e){ e.preventDefault(); openIframe(url4); }); // Note: Uses the same URL as btn4
+        btnTku.addEventListener('click', function(e){ e.preventDefault(); openIframe(urlTku); });
+        btnTkuEt.addEventListener('click', function(e){ e.preventDefault(); openIframe(urlTkuEt); });
+        closeBtn.addEventListener('click', function(e){ e.stopPropagation(); closeIframe(); });
+
+        // Click outside container closes
+        overlay.addEventListener('click', function(e){ if(e.target === overlay) closeIframe(); });
+      })();
+    </script>
+    <script src="sketch.js"></script>
+  </body>
+</html>
